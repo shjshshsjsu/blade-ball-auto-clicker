be7a52dde967bba79cd95b9296f621ce2b6c22e5
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-10-17 01:45:50 +0300

    Update autoclick.lua

--- autoclick.lua
+++ autoclick.lua
@@ -1,7 +1,4 @@
-local dragging
-local dragInput
-local dragStart
-local startPos
+
 
 -- UI
 local main = Instance.new("ScreenGui")
@@ -81,33 +78,3 @@ Toogle.MouseButton1Click:Connent(function()
     end
   end)
 
-local function updateInput(input)
-    local delta = input.Position - dragStart
-    Frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-end
-
-Frame.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        dragging = true
-        dragStart = input.Position
-        startPos = Frame.Position
-
-        input.Changed:Connect(function()
-            if input.UserInputState == Enum.UserInputState.End then
-                dragging = false
-            end
-        end)
-    end
-end)
-
-Frame.InputChanged:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-        dragInput = input
-    end
-end)
-
-game:GetService("UserInputService").InputChanged:Connect(function(input)
-    if input == dragInput and dragging then
-        updateInput(input)
-    end
-end)
